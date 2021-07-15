d4395a0f03d0d1c9122c29ea1ad1632e2bb36d5a
Егор <it.is.mr.lag@gmail.com>
2021-07-15 09:09:05 +0300

    Added classes EasyOcr and OSRReader.
    EasyOcr - class for finding number on the frame 
    OSRReader - class for displaying and saving the results of the previous class

--- models.py
+++ models.py
@@ -2,6 +2,11 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from easyocr import Reader
+import numpy as np
+import time
+from utils import warp_image, show_image, get_filelist, draw_bbox
+import cv2
+import pytesseract
 
 
 class CNN(nn.Module):
@@ -83,19 +88,159 @@ class MLP(nn.Module):
 
 
 class EasyOcr:
-
+    """class with EasyOcr and sorting algorithm"""
     def __init__(self):
         self.model = Reader(["en"], gpu=True, verbose=False)
+        self.buff = {}
+        self.ID = []
 
-    def predict(self, image):
-        results = self.model.readtext(image)
+    def predict(self, image, minsize):
+        """
+        minsize - minimum numbers of the answer that would be used in the final choosing
 
-        res = {"bbox": None, "number": None, "prob": None}
+        return info - dict with keys (bbox - bounding box, prob - prediction, number - text, frame - picture)
+        """
+        results = self.model.readtext(
+            image,
+            contrast_ths=10,
+            low_text=0.15,
+        )
+        all_items = {}
+        clefs = []
+        ans = {}
         for (bbox, text, prob) in results:
             (tl, tr, br, bl) = bbox
             tl = (int(tl[0]), int(tl[1]))
             tr = (int(tr[0]), int(tr[1]))
             br = (int(br[0]), int(br[1]))
             bl = (int(bl[0]), int(bl[1]))
+            res = {"bbox": (tr, tl, br, bl), "number": text, "prob": prob, "frame": image}
 
-        return res
+            clef = tr[0] - tl[0]
+            all_items[clef] = res
+            if len(text) < minsize:
+                continue
+            else:
+                clefs.append(clef)
+        clefs.sort()
+        if len(clefs) > 0:
+            ans = all_items[clefs[-1]]
+        return ans
+
+    def saver(self, ans):
+        self.buff[ans["prob"]] = ans
+        self.ID.append(ans["prob"])
+        # print(self.ID)
+
+    def choose(self):
+        if self.ID:
+            self.ID.sort()
+            id = self.ID[-1]
+            ans = self.buff[id]
+            self.buff.clear()
+            self.ID.clear()
+            return ans
+
+    def cleaner(self):
+        self.buff.clear()
+        self.ID.clear()
+
+
+class OCRReader:
+    def __init__(self, src, type="mp4", format_directory="jpg", all_info=None):
+        """
+        ---this class should be used for comfortable reading text-information from frames---
+
+        > There are three types ["mp4" or "rtsp", "img", "dir"] - video, image, directory.
+        (format_directory - it is name-type of all images in the directory)
+
+        > all_info - dict, list, tuple or something else (structure where would be saved all info)
+        """
+        self.type = type
+        self.src = src
+        self.all_info = all_info
+        self.video = None
+        self.empty_frames = 0
+        self.model = EasyOcr()
+        if type == "mp4" or type == "rtsp":
+            self.video = cv2.VideoCapture(src)
+        elif type == "dir":
+            self.counter = 0
+            self.imgs = get_filelist(src, [format_directory])
+        elif type == "img":
+            self.img = cv2.imread(src)
+        else:
+            raise Exception("Not implemented reader type")
+
+    def video_run(self, max_wait_iterations=20, cut_box=[(196, 400), (1235, 400), (1235, 1041), (196, 1041)], watch=True):
+        """
+        > max_wait_iterations - delay after choosing final info for saving
+        > watch - (True or False) watching video
+        > cut_box - coordinates in pixels for cutting frame [(top_left), (top_right), (bot_right), (bot_left)] (x, y)
+        """
+        _, img = self.video.read()
+        warped_img = warp_image(img, np.array(eval(str(cut_box)), dtype="float32"))
+        results = self.model.predict(warped_img, 7)  # , draw_bbox
+        if len(results) == 0:
+            self.empty_frames += 1
+        if len(results):
+            self.model.saver(results)
+            self.image_show(warped_img, results)
+        elif self.empty_frames == max_wait_iterations:
+            pif_paf = self.model.choose()
+            if pif_paf:
+                self.all_info[time.time()] = pif_paf
+                print(self.empty_frames)
+            self.empty_frames = 0
+        if watch:
+            show_image(warped_img, delay=0)
+
+    def image_run(self, cut_box=[(196, 400), (1235, 400), (1235, 1041), (196, 1041)], watch=True):
+        """
+        > watch - (True or False) watching image
+        > cut_box - coordinates in pixels for cutting frame [(top_left), (top_right), (bot_right), (bot_left)] (x, y)
+        """
+        warped_img = warp_image(self.img, np.array(eval(str(cut_box)), dtype="float32"))
+        results = self.model.predict(warped_img, 7)  # , draw_bbox
+        if len(results):
+            self.image_show(warped_img, results)
+            self.all_info[time.time()] = results
+            print(self.all_info)
+        if watch:
+            show_image(warped_img, delay=0)
+
+    def images_run(self, cut_box=[(196, 400), (1235, 400), (1235, 1041), (196, 1041)], watch=True):
+        """
+        > watch - (True or False) watching images from directory
+        > cut_box - coordinates in pixels for cutting frame [(top_left), (top_right), (bot_right), (bot_left)] (x, y)
+        """
+        try:
+            img = cv2.imread(self.imgs[self.counter])
+        except:
+            exit()
+        self.counter += 1
+        warped_img = warp_image(img, np.array(eval(str(cut_box)), dtype="float32"))
+        results = self.model.predict(warped_img, 7)  # , draw_bbox
+        if not (len(results)):
+            self.empty_frames += 1
+        if len(results):
+            self.model.saver(results)
+            self.image_show(warped_img, results)
+        elif self.empty_frames == 1:
+            pif_paf = self.model.choose()
+            if pif_paf:
+                self.all_info[time.time()] = pif_paf
+                print(self.empty_frames)
+            self.empty_frames = 0
+        if watch:
+            show_image(warped_img, delay=0)
+
+    def image_show(self, img, results):
+        tl = results["bbox"][1]
+        br = results["bbox"][2]
+        text = results["number"]
+        cv2.rectangle(img, tl, br, (0, 255, 0), 2)
+        cv2.putText(img, text, (tl[0], tl[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
+
+    def cleaner(self):
+        self.empty_frames = 0
--- test_ocr.py
+++ test_ocr.py
@@ -3,52 +3,54 @@ import cv2
 import numpy as np
 import time
 from models import EasyOcr
-from utils import warp_image, show_image
+from utils import warp_image, show_image, get_filelist, draw_bbox
 
 
-# img = cv2.imread("/home/ea/projects/SIRIUS21/data/1_7b8pORMeluIx7XG-smavDQ.png")
-img = cv2.imread("photo_2021-07-10_12-12-37.jpg")
+empty_frames = 0   # frames without numbers
+cc = 0
+
+imgs = get_filelist("/home/home/projects/neural_backend/samples/ocr", ["jpg"])
+# img = cv2.imread("/home/home/projects/neural_backend/samples/numberOCR.jpg")
+# img = cv2.imread("photo_2021-07-10_12-12-37.jpg")
 # img = cv2.imread("photo_2021-07-10_12-18-24.jpg")
 # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-v = "./samples/numberOCR.mp4"
+v = "./samples/cutVideo.mp4"
 
 cap = cv2.VideoCapture(v)
 
 model = EasyOcr()
-
+all_info = {}     # dict which is considered all frames with numbers and their optional information
 while True:
 
-    _, img = cap.read()
+    try:
+        img = cv2.imread(imgs[cc])
+    except:
+        exit()
+    cc += 1
 
-    warped_img = img
-    # warped_img = warp_image(img, np.array(eval(str([(215, 686), (788, 468),(789, 682), (290, 953)])),dtype="float32"))
-
-    results = model.predict(warped_img)
-    for (bbox, text, prob) in results:
-
-        (tl, tr, br, bl) = bbox
-        tl = (int(tl[0]), int(tl[1]))
-        tr = (int(tr[0]), int(tr[1]))
-        br = (int(br[0]), int(br[1]))
-        bl = (int(bl[0]), int(bl[1]))
-
+    # _, img = cap.read()  #TODO
+    # warped_img = img
+    warped_img = warp_image(img, np.array(eval(str([(196, 400), (1235, 400), (1235, 1041), (196, 1041)])), dtype="float32"))
+    results = model.predict(warped_img, 7)  #, draw_bbox
+    if not(len(results)):
+        empty_frames += 1
+    if len(results):
+        model.saver(results)
+        # print("results - ", results)
+        tl = results["bbox"][1]
+        br = results["bbox"][2]
+        text = results["number"]
         cv2.rectangle(warped_img, tl, br, (0, 255, 0), 2)
-        cv2.putText(warped_img, text, (tl[0], tl[1] - 10),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
+        cv2.putText(warped_img, text, (tl[0], tl[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
+        # print(empty_frames)
+    elif empty_frames == 20:
+        pif_paf = model.choose()
+        if pif_paf:
+            all_info[time.time()] = pif_paf
+        empty_frames = 0
+        print(all_info)
 
-    show_image(warped_img)
+    show_image(warped_img, delay=0)
 
-
-# results = reader.readtext(image)
-
-# cv2.imwrite("/home/ea/projects/SIRIUS21/ocr.pytorch/test_images/tt.png", warped_img)
-
-# warped_img = cv2.cvtColor(warped_img, cv2.COLOR_RGB2GRAY)
-
-# warped_img = enlarge_img(warped_img, 200)
-
-# print(pytesseract.image_to_string(warped_img, config = r'--psm 8'))
-#
-# show_image(warped_img)
 
